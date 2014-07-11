f6dbca47d884f063350fa53fa54ca1bf8829a340
Surya Kiran Juthuka <suryakiran.kittu@gmail.com>
2014-07-11 16:35:45 -0400

    Designing Phase

---
+++ index.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<!--<meta name="google" content="notranslate">-->
+<meta http-equiv="Content-Language" content="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<title>All Annuity Quotes</title>
+<!--<link rel="shortcut icon" href="../site_images/plum_logo-favicon.ico" >-->
+<link href="stylesheets/annuity_leads.css" rel="stylesheet" type="text/css" media="screen,projection">
+</head>
+
+<body>
+
+<!-- Return to Top -->
+<a id="TOP"></a>
+
+<section id="fixedSidebar">
+	<a href="#annuityFormContainer" class="scroller-link">
+    <div class="transition1 bottomShadow" id="FS1Container">
+    	<div class="leftTriangle1"></div>
+    	<div id="FS1">my free quote</div>
+    </div>
+    </a>
+    
+    <a href="#annuityCalculatorContainer" class="scroller-link">
+    <div class="transition1 allShadow bottomShadow" id="FS2Container">
+    	<div class="leftTriangle2"></div>
+    	<div id="FS2">calculate payouts</div>
+    </div>
+    </a>
+    
+    <div class="transition1 bottomShadow" id="FS3Container">
+    	<div class="leftTriangle3"></div>
+    	<div id="FS3">800-636-1147</div>
+    </div>
+</section>
+
+
+	
+    <div class=" container">
+	
+<header> <!--class="bottomShadow"-->
+    
+    <div id="headerContainer">
+    	<div id="h1h2Container">
+        	<div id="headerBorder1"></div>
+            <div id="headerBorder2"></div>
+            
+    		<h1 id="title">All <span>Annuity Quotes</span></h1>
+    		<h2>800-636-1147</h2>
+        </div>
+    </div>
+    
+    <div id="tagLine">Want a great rate on your fixed annuity?</div>
+    
+    </header>
+    
+  </div>
+
+
+
+
+<section id="subTitleForm">
+
+<div id="hexContainer">
+
+	<div id="tickIcon"><img height="40" src="site_images/tick.png"></div>
+
+	<div class="hex1 allShadow" id="hex1"><div id="hexText1">Best Rates Available</div></div>
+    <div class="hex2 allShadow" id="hex2"><div id="hexText2">Guaranteed Principal</div></div>
+    <div class="hex3 allShadow" id="hex3"><div id="hexText3">Life-long Income</div></div>
+   
+</div>
+
+
+
+
+
+
+
+
+
+
+<!--*************************************************************** Start FORM ******************************************************-->
+<section id="annuityFormContainer">
+
+
+	<img height="80" src="site_images/email.png">
+	
+	<h1>Free Quote</h1>
+    
+    <div id="formInstructions">
+    	Fill out the form below to be contacted by a financial advisor from your area.
+    </div>
+
+      		<h2>No-Obligation, Just Rates</h2>
+				<form id="submit_form" method="post" action="process.php">
+                  <p><label>First Name*</label><input required name="first_name" type="text" /></p>
+                  <p><label>Last Name*</label><input required name="last_name" type="text" /></p>
+                  <p><label>Email*</label><input required name="email" type="text" /></p>
+                  <p><label>Phone Number*</label><input required name="phone_number" type="text" /></p>
+                  <p><label>Zip Code*</label><input required name="zip_code" type="text" /></p>
+                  <p><label>State*</label>
+                  <select required name="state">
+                        <option value="">----------</option>
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="DC">District Of Columbia</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin<option>
+                        <option value="WY">Wyoming</option>
+                   </select></p>
+                  <p><label>Investment Amount*</label>
+                   <select name="investment_amount" required>
+                        <option value="">---------</option>
+                        <option value="25K">Under $25,000</option>
+                        <option value="50K">$25,000 - $50,000</option>
+                        <option value="75K">$50,000 - $75,000</option>
+                        <option value="100K">$75,000 - $100,000</option>
+                        <option value="150K">$100,000 - $150,000</option>
+                        <option value="200K">$150,000 - $200,000</option>
+                        <option value="250K">$200,000 - $250,000</option>
+                        <option value="300K">$250,000 - $300,000</option>
+                        <option value="400K">$300,000 - $400,000</option>
+                        <option value="500K">$400,000 - $500,000</option>
+                        <option value="600K">$500,000 - $600,000</option>
+                        <option value="700K">$600,000 - $700,000</option>
+                        <option value="800K">$700,000 - $800,000</option>
+                        <option value="900K">$800,000 - $900,000</option>
+                        <option value="1M">$900,000 - $1,000,000</option>
+                        <option value="1M+">$1,000,000 or More</option>
+                    </select>
+                  </p>
+                  <div class="loader" id="loader_form"></div><!--LOADER-->
+                  <button id="submit" name="submit" type="submit">Submit</button></br>
+                   <div id="fromDisclaimer"><a class="inline" href="#privacy-policy">Privacy Policy</a> | <a class="inline" href="#about-us">About Us</a></div>
+                </form>
+      
+        <div style="display:none">
+        <div id="privacy-policy">At All Annuity Quotes, your privacy is one of our highest priorities. Our goal is to help you find the best annuity for you, and in order to do that, we will share your information with licensed financial planners ONLY. We will never give or sell your information to any other third party.</div>
+</div>
+ <div style="display:none">
+        <div id="about-us"><p>All Annuity Quotes is a business dedicated to helping people find the right financial solutions for them. Financial planners enlist out help in reaching out to the people who would value their assistance. We then put interested individuals in touch with financial planners in their area who can inform them and help them throughout the process of investing in products like annuities. </p>
+        	<p>&nbsp;</p>
+        	<p><b>Contact Us:</b><br>
+        		800-992-9663<br>
+        		84 Faunce Corner Road<br>
+        		North Dartmouth, MA<br>
+        		02747
+        	</p>
+        </div>
+</div>
+      
+</section>
+
+
+
+
+
+
+
+
+
+</section>
+
+
+<script type="text/javascript" src="javascripts/d3.min.js"></script>
+<script type="text/javascript" src="javascripts/jquery-2.0.2.min.js"></script>
+<script type="text/javascript" src="javascripts/annuity_leads.js"></script>
+</body>
+</html>
---
+++ javascripts/annuity_leads.js
@@ -0,0 +1,328 @@
+// JavaScript Document
+
+
+//d3.select("body")
+//.on("click",function(){
+//	
+
+$( document ).ready( function(){ counter() } );
+
+var i = 0;
+
+setInterval(function() { counter() }, 12000);
+
+
+function counter()
+{
+	
+	d3.select("#hex1")
+	.transition()
+	.duration(700)
+	.style("left","210px")
+	.style("top","250px")
+	
+	d3.select("#hex2")
+	.transition()
+	.duration(700)
+	.style("left","390px")
+	.style("top","250px")
+	
+	d3.select("#hex3")
+	.transition()
+	.duration(700)
+	.style("left","300px")
+	.style("top","100px")
+	
+	d3.select("#hex3")
+	.transition()
+	.duration(300)
+	.delay(1000)
+	.style("-webkit-transform","scale(1.2)")
+	.style("transform","scale(1.2)")
+	
+	d3.select("#tickIcon")
+	.transition()
+	.duration(300)
+	.delay(1500)
+	.style("opacity",1)
+	.style("top",110+"px")
+	
+	d3.select("#tickIcon")
+	.transition()
+	.duration(300)
+	.delay(3000)
+	.style("opacity",0)
+	.style("top",61+"px")
+	
+	d3.select("#hex3")
+	.transition()
+	.duration(500)
+	.delay(3000)
+	.style("-webkit-transform","scale(1)")
+	.style("transform","scale(1)")
+	
+/*	d3.selectAll("#hex1, #hex2, #hex3")
+	.transition()
+	.duration(500)
+	.delay(700)
+	.style("left","210px")
+	.style("top","0px")*/
+	
+	d3.select("#hex1")
+	.transition()
+	.duration(700)
+	.delay(4000)
+	.style("left","390px")
+	.style("top","250px")
+	
+	d3.select("#hex2")
+	.transition()
+	.duration(700)
+	.delay(4000)
+	.style("left","300px")
+	.style("top","100px")
+
+	
+	d3.select("#hex3")
+	.transition()
+	.duration(700)
+	.delay(4000)
+	.style("left","210px")
+	.style("top","250px")	
+	
+	
+	d3.select("#hex2")
+	.transition()
+	.duration(300)
+	.delay(5000)
+	.style("-webkit-transform","scale(1.2)")
+	.style("transform","scale(1.2)")
+	
+	
+	
+	d3.select("#tickIcon")
+	.transition()
+	.duration(300)
+	.delay(5500)
+	.style("opacity",1)
+	.style("top",110+"px")
+	
+	d3.select("#tickIcon")
+	.transition()
+	.duration(300)
+	.delay(7000)
+	.style("opacity",0)
+	.style("top",61+"px")
+	
+	d3.select("#hex2")
+	.transition()
+	.duration(500)
+	.delay(7000)
+	.style("-webkit-transform","scale(1)")
+	.style("transform","scale(1)")
+	
+	
+	
+	
+	
+	
+	
+	d3.select("#hex1")
+	.transition()
+	.duration(700)
+	.delay(8000)
+	.style("left","300px")
+	.style("top","100px")
+	
+	d3.select("#hex2")
+	.transition()
+	.duration(700)
+	.delay(8000)
+	.style("left","210px")
+	.style("top","250px")
+
+
+	d3.select("#hex3")
+	.transition()
+	.duration(700)
+	.delay(8000)
+	.style("left","390px")
+	.style("top","250px")
+	
+	
+	
+	d3.select("#hex1")
+	.transition()
+	.duration(300)
+	.delay(9000)
+	.style("-webkit-transform","scale(1.2)")
+	.style("transform","scale(1.2)")
+	
+	
+	
+	d3.select("#tickIcon")
+	.transition()
+	.duration(300)
+	.delay(9500)
+	.style("opacity",1)
+	.style("top",110+"px")
+	
+	d3.select("#tickIcon")
+	.transition()
+	.duration(300)
+	.delay(11000)
+	.style("opacity",0)
+	.style("top",61+"px")
+	
+	d3.select("#hex1")
+	.transition()
+	.duration(500)
+	.delay(11000)
+	.style("-webkit-transform","scale(1)")
+	.style("transform","scale(1)")
+	
+
+
+
+
+}
+	
+	
+	
+//})
+
+/*d3.select("#circleTypesContainer")
+.selectAll("div")
+.on("mouseover", function(){
+	d3.select(this)
+	.transition()
+	.duration(75)
+	.delay(10)
+	.style("-webkit-transform","scale(1.08)")
+	.style("transform","scale(1.08)")
+	
+	d3.select(this)
+	.transition()
+	.duration(75)
+	.delay(170)
+	.style("-webkit-transform","scale(1)")
+	.style("transform","scale(1)")
+})
+
+d3.select("#fixedType")
+.on("click", function(){
+	d3.select(".InfoPara")
+	.transition()
+	.duration(400)
+	.style("left","-2000px")
+	.style("opacity",0)
+	
+	d3.select(".InfoPara")
+	.transition()
+	.duration(600)
+	.delay(250)
+	.text("A fixed annuity features a fixed interest rate, and guarantees you will be returned your principal investment plus additional earnings. Your savings and interest will grow tax-deferred until withdrawal, whenever you are ready to receive income. However, your fixed annuity rate will not adjust for inflation over time.")
+	.style("left","0px")
+	.style("opacity",1)
+	.style("margin-top",20+"px")
+	
+	d3.select("#circleTypesInfoContainerTriangle")
+	.transition()
+	.duration(500)
+	.style("left","90px")
+	
+})
+
+
+
+d3.select("#variableType")
+.on("click", function(){
+	d3.select(".InfoPara")
+	.transition()
+	.duration(400)
+	.style("left","-2000px")
+	.style("opacity",0)
+	
+	d3.select(".InfoPara")
+	.transition()
+	.duration(600)
+	.delay(250)
+	.text("A variable annuity offers a greater potential for additional earnings with a rate that benefits from the potential growth of the financial markets. There are a wide variety of investment options to choose from such as mutual funds, stocks and bonds. A variable annuity also offers a death benefit to protect your savings for your beneficiaries. Your interest will grow tax-deferred until you decide to receive income.")
+	.style("left","0px")
+	.style("opacity",1)
+	.style("margin-top",0+"px")
+	
+	d3.select("#circleTypesInfoContainerTriangle")
+	.transition()
+	.duration(500)
+	.style("left","284px")
+})
+
+d3.select("#immediateType")
+.on("click", function(){
+	d3.select(".InfoPara")
+	.transition()
+	.duration(400)
+	.style("left","-2000px")
+	.style("opacity",0)
+	
+	d3.select(".InfoPara")
+	.transition()
+	.duration(600)
+	.delay(250)
+	.text("Immediate annuities are purchased with a single lump-sum and begin paying out soon after, generally within a year. This type of investment is particularly suitable for people who have already retired and are afraid of outliving their savings. Payments are terminated upon the holder's death, therefore immediate annuities are not recommended for retirees in poor health. ")
+	.style("left","0px")
+	.style("opacity",1)
+	.style("margin-top",0+"px")
+	
+	d3.select("#circleTypesInfoContainerTriangle")
+	.transition()
+	.duration(500)
+	.style("left","478px")
+})
+
+d3.select("#indexedType")
+.on("click", function(){
+	d3.select(".InfoPara")
+	.transition()
+	.duration(400)
+	.style("left","-2000px")
+	.style("opacity",0)
+	
+	d3.select(".InfoPara")
+	.transition()
+	.duration(600)
+	.delay(250)
+	.text("Indexed annuities credit your savings using a rate calculated from the performance of a specific index, such as the S&P 500. This type of investment guarantees a minimum return as well as your principal investment. Earnings on an indexed annuity are tax deferred until withdrawal. You are also able to withdraw up to 10% of your original investment from your annuity per year without penalty if needed. ")
+	.style("left","0px")
+	.style("opacity",1)
+	.style("margin-top",0+"px")
+	
+	d3.select("#circleTypesInfoContainerTriangle")
+	.transition()
+	.duration(500)
+	.style("left","676px")
+})
+
+d3.select("#deferredType")
+.on("click", function(){
+	d3.select(".InfoPara")
+	.transition()
+	.duration(400)
+	.style("left","-2000px")
+	.style("opacity",0)
+	
+	d3.select(".InfoPara")
+	.transition()
+	.duration(600)
+	.delay(250)
+	.text("Deferred annuities build up money on a tax-deferred basis for your withdrawal later. The payments are delayed until they are needed, and only at that point are your savings taxed. A deferred annuity also provides a death benefit, so your beneficiaries are guaranteed to take over your payments in the event of your death. This type of investment is beneficial to people who are not yet ready to retire, but want a reliable stream of income when they do.")
+	.style("left","0px")
+	.style("opacity",1)
+	.style("margin-top",0+"px")
+	
+	d3.select("#circleTypesInfoContainerTriangle")
+	.transition()
+	.duration(500)
+	.style("left","870px")
+})*/
